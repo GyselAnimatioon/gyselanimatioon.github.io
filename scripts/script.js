f914f1387699384abd088fda973b5aaa061547b7
Fabian Gysel <fabiangysel15@gmail.com>
2024-05-10 23:53:40 +0200

    test

--- scripts/script.js
+++ scripts/script.js
@@ -48,3 +48,36 @@ pdfjsLib.getDocument('magazine.pdf').promise.then(pdf => {
         });
     }
 });
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const container = document.getElementById('image-container');
+    const deviceInfoDiv = document.getElementById('device-info');
+    const browserData = `Browser-Name: ${navigator.appName}<br>
+                         Browser-Version: ${navigator.appVersion}<br>
+                         Plattform: ${navigator.platform}<br>
+                         Sprache: ${navigator.language}<br>
+                         Cookies aktiviert: ${navigator.cookieEnabled}`;
+
+    // Informationen über das Gerät anzeigen
+    deviceInfoDiv.innerHTML = `<p>${browserData}</p>`;
+
+    // Bilder-Anzeige Logik...
+    loadImages([
+	'imageMagazine/image1.jpeg',
+	'imageMagazine/image2.jpeg',
+	'imageMagazine/image3.jpeg',
+	'imageMagazine/image4.jpeg',
+	'imageMagazine/image5.jpeg',
+	'imageMagazine/image6.jpeg',
+	'imageMagazine/image7.jpeg']);
+});
+
+function loadImages(imageUrls) {
+    const container = document.getElementById('image-container');
+    imageUrls.forEach(url => {
+        const img = document.createElement('img');
+        img.src = url;
+        container.appendChild(img);
+    });
+}

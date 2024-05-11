853a00ccf82386b4140a7e82dc73c8c167233bc8
Fabian Gysel <fabiangysel15@gmail.com>
2024-05-11 11:10:00 +0200

    test

--- scripts/script.js
+++ scripts/script.js
@@ -1,48 +1,36 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const deviceInfoDiv = document.createElement('div');
-    deviceInfoDiv.id = 'device-info';
-    const browserData = `Browser-Name: ${navigator.appName}<br>
-                         Browser-Version: ${navigator.appVersion}<br>
-                         Plattform: ${navigator.platform}<br>
-                         Sprache: ${navigator.language}<br>
-                         Cookies aktiviert: ${navigator.cookieEnabled}`;
-    deviceInfoDiv.innerHTML = `<p>${browserData}</p>`;
-    document.body.appendChild(deviceInfoDiv);  // Stellen Sie sicher, dass dies Teil Ihres Dokuments ist
+    const loader = document.getElementById('loader');
+    const container = document.getElementById('pdf-container');
+    const isMobile = window.innerWidth < window.innerHeight;
 
-    // PDF-Anzeige Logik...
-});
+    pdfjsLib.getDocument('magazine.pdf').promise.then(pdf => {
+        loader.style.display = 'none';  // Ladebildschirm ausblenden
 
-const loader = document.getElementById('loader');
-const container = document.getElementById('pdf-container');
-const isMobile = window.innerWidth < window.innerHeight;
+        for (let pageNum = 1; pageNum <= pdf.numPages; pageNum++) {
+            let canvas = document.createElement('canvas');
+            canvas.id = `pdf-renderer-${pageNum}`;
+            container.appendChild(canvas);
 
-pdfjsLib.getDocument('magazine.pdf').promise.then(pdf => {
-    //loader.style.display = 'none';  // Ladebildschirm ausblenden
+            pdf.getPage(pageNum).then(page => {
+                var viewport = page.getViewport({scale: 1});
+                canvas.width = viewport.width;
+                canvas.height = viewport.height;
 
-    for (let pageNum = 1; pageNum <= pdf.numPages; pageNum++) {
-        let canvas = document.createElement('canvas');
-        canvas.id = `pdf-renderer-${pageNum}`;
-        container.appendChild(canvas);
+                if (isMobile) {
+                    canvas.style.width = "100%";
+                } else {
+                    canvas.style.maxWidth = "100%";
+                }
 
-        pdf.getPage(pageNum).then(page => {
-            var viewport = page.getViewport({scale: 1});
-            canvas.width = viewport.width;
-            canvas.height = viewport.height;
-
-            if (isMobile) {
-                canvas.style.width = "100%";
-            } else {
-                canvas.style.maxWidth = "100%";
-            }
-
-            var renderContext = {
-                canvasContext: canvas.getContext('2d'),
-                viewport: viewport
-            };
-            page.render(renderContext);
-        });
-    }
-}).catch(error => {
-    console.error('Fehler beim Laden des PDFs: ', error);
-    loader.innerHTML = '<p>Fehler beim Laden des PDFs. Bitte versuchen Sie es später erneut.</p>';
+                var renderContext = {
+                    canvasContext: canvas.getContext('2d'),
+                    viewport: viewport
+                };
+                page.render(renderContext);
+            });
+        }
+    }).catch(error => {
+        console.error('Fehler beim Laden des PDFs: ', error);
+        loader.innerHTML = '<p>Fehler beim Laden des PDFs. Bitte versuchen Sie es später erneut.</p>';
+    });
 });

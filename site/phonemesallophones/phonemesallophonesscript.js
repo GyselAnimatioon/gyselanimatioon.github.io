2e957ee83bce5f70330b4d9918ca4c635d62bf94
Fabian Gysel <fabiangysel15@gmail.com>
2024-05-12 16:51:29 +0200

    CHANGES

--- site/phonemesallophones/phonemesallophonesscript.js
+++ site/phonemesallophones/phonemesallophonesscript.js
@@ -43,7 +43,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
                         // Nach dem Rendern der 5. Seite, fügen Sie den HTML-Teil ein, bevor die 6. Seite geladen wird
                         if (pageNum === 5) {
                             let htmlContent = document.createElement('div');
-                            htmlContent.innerHTML = '<h2>Related Audios</h2> <div class="audio-container"> <audio controls> <source src="audio1.mp3" type="audio/mpeg"> Ihr Browser unterstützt das Audio-Element nicht. </audio> <audio controls> <source src="audio2.mp3" type="audio/mpeg"> Ihr Browser unterstützt das Audio-Element nicht. </audio> <audio controls> <source src="audio3.mp3" type="audio/mpeg"> Ihr Browser unterstützt das Audio-Element nicht. </audio> </div> <br>';
+                            htmlContent.innerHTML = '<br><h2>Related Audios</h2> <div class="audio-container"> <audio controls> <source src="audio1.mp3" type="audio/mpeg"> Ihr Browser unterstützt das Audio-Element nicht. </audio> <audio controls> <source src="audio2.mp3" type="audio/mpeg"> Ihr Browser unterstützt das Audio-Element nicht. </audio> <audio controls> <source src="audio3.mp3" type="audio/mpeg"> Ihr Browser unterstützt das Audio-Element nicht. </audio> </div> <br><br>';
                             container.appendChild(htmlContent);
                         }
 
